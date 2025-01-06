e59c7b74d4fbe8b7a2f51519276dbd723e309fe8
techycereal <alexjmilliken@outlook.com>
2025-01-06 10:59:03 -0500

    update

--- src/components/AuthSuccess.jsx
+++ src/components/AuthSuccess.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { setAuth } from '../features/authSlice/authSlice';
 const AuthSuccess = () => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch()
   useEffect(() => {
     // Get the token from the URL
     console.log('here')
     const queryParams = new URLSearchParams(window.location.search);
     const token = queryParams.get('token');
-    console.log(token)
     if (token) {
       // Store the token in localStorage
       localStorage.setItem('authToken', token);
-
+      dispatch(setAuth(true))
       // Redirect to the CreateEvent page
       navigate('/create-event');
     } else {
